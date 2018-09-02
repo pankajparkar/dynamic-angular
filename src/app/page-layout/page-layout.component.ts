7f728f187f64428f85e8d342dbe621fb9239469f
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-09-02 21:58:26 +0530

    Changed the alignment and added object of form that to be render dynamically.

--- src/app/page-layout/page-layout.component.ts
+++ src/app/page-layout/page-layout.component.ts
@@ -7,6 +7,17 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
   styleUrls: ['./page-layout.component.css']
 })
 export class PageLayoutComponent implements OnInit {
+
+  formDynamicInput:any[] = [
+    {id: 1, fieldName: 'firstName', label: 'First Name', fieldType: 'input', validation: ['Required']},
+    {id: 2, fieldName: 'lastName', label: 'Last Name', fieldType: 'input', validation: ['Required']},
+    {id: 3, fieldName: 'company', label: 'Company', fieldType: 'input', validation: ['Required']},
+    {id: 4, fieldName: 'address', label: 'Address', fieldType: 'textarea', validation: ['Required']},
+    {id: 5, fieldName: 'address2', label: 'Address 2', fieldType: 'textarea', validation: ['Required']},
+    {id: 6, fieldName: 'city', label: 'City', fieldType: 'input', validation: ['Required']},
+    {id: 7, fieldName: 'state', label: 'State', fieldType: 'input', validation: ['Required']},
+    {id: 8, fieldName: 'postalCode', label: 'Postal Code', fieldType: 'input', validation: ['Required']}
+  ]
   
   userForm: FormGroup;
 
