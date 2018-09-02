9fb31b246ca881156e1061a2da7251df976bc606
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-09-02 12:15:05 +0530

    Added form field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatInputModule } from '@angular/material';
 import { PageLayoutComponent } from './page-layout/page-layout.component';
 
 @NgModule({
@@ -17,6 +17,7 @@ import { PageLayoutComponent } from './page-layout/page-layout.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatInputModule,
     LayoutModule,
     MatToolbarModule,
     MatButtonModule,
