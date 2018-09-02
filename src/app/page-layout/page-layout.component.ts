c00a2f3d6e68938757b42c39383db6a1a62df094
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-09-03 00:09:16 +0530

    Dynamic form completely replaced

--- src/app/page-layout/page-layout.component.ts
+++ src/app/page-layout/page-layout.component.ts
@@ -7,7 +7,7 @@ import { FormBuilder, FormGroup, FormControl, Validators, FormArrayName } from '
   styleUrls: ['./page-layout.component.css']
 })
 export class PageLayoutComponent implements OnInit {
-
+  dynamicFormGroup: FormGroup;
   formDynamicInput:any[] = [
     {id: 1, fieldName: 'firstName', label: 'First Name', fieldType: 'input', placeholder: 'First Name', validation: ['Required'], value: '3453'},
     {id: 2, fieldName: 'lastName', label: 'Last Name', fieldType: 'input', placeholder: 'Long Last Name That Will Be Truncated', validation: ['Required'], value: ''},
@@ -19,23 +19,9 @@ export class PageLayoutComponent implements OnInit {
     {id: 8, fieldName: 'postalCode', label: 'Postal Code', fieldType: 'input', placeholder: 'Postal Code', validation: ['Required'], value: ''}
   ]
   
-  userForm: FormGroup;
-  dynamicFormGroup: FormGroup;
-
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.userForm = this.formBuilder.group({
-      firstName: new FormControl('', Validators.required),
-      lastName: new FormControl('', Validators.required),
-      company: new FormControl('', Validators.required),
-      address: new FormControl('', Validators.required),
-      address2: new FormControl('', Validators.required),
-      city: new FormControl('', Validators.required),
-      state: new FormControl('', Validators.required),
-      postCode: new FormControl('', Validators.required)
-    })
-
     const dynamicFormInputs = {};
     this.formDynamicInput.forEach(({fieldName, fieldType, value}) => {
       dynamicFormInputs[fieldName] = new FormControl(value, Validators.required)
