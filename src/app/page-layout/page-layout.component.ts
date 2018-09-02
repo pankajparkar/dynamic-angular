9825780320b920748e7568b9e3cd41fe4ffdf4e0
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-09-02 23:41:53 +0530

    Dynamically rendered form based on JSON.

--- src/app/page-layout/page-layout.component.ts
+++ src/app/page-layout/page-layout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators, FormArrayName } from '@angular/forms';
 
 @Component({
   selector: 'da-page-layout',
@@ -9,17 +9,18 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
 export class PageLayoutComponent implements OnInit {
 
   formDynamicInput:any[] = [
-    {id: 1, fieldName: 'firstName', label: 'First Name', fieldType: 'input', validation: ['Required']},
-    {id: 2, fieldName: 'lastName', label: 'Last Name', fieldType: 'input', validation: ['Required']},
-    {id: 3, fieldName: 'company', label: 'Company', fieldType: 'input', validation: ['Required']},
-    {id: 4, fieldName: 'address', label: 'Address', fieldType: 'textarea', validation: ['Required']},
-    {id: 5, fieldName: 'address2', label: 'Address 2', fieldType: 'textarea', validation: ['Required']},
-    {id: 6, fieldName: 'city', label: 'City', fieldType: 'input', validation: ['Required']},
-    {id: 7, fieldName: 'state', label: 'State', fieldType: 'input', validation: ['Required']},
-    {id: 8, fieldName: 'postalCode', label: 'Postal Code', fieldType: 'input', validation: ['Required']}
+    {id: 1, fieldName: 'firstName', label: 'First Name', fieldType: 'input', placeholder: 'First Name', validation: ['Required'], value: '3453'},
+    {id: 2, fieldName: 'lastName', label: 'Last Name', fieldType: 'input', placeholder: 'Long Last Name That Will Be Truncated', validation: ['Required'], value: ''},
+    {id: 3, fieldName: 'company', label: 'Company', fieldType: 'input', placeholder: 'Company', validation: ['Required'], value: ''},
+    {id: 4, fieldName: 'address', label: 'Address', fieldType: 'textarea', placeholder: 'Address', validation: ['Required'], value: ''},
+    {id: 5, fieldName: 'address2', label: 'Address 2', fieldType: 'textarea',placeholder: 'Address 2', validation: ['Required'], value: ''},
+    {id: 6, fieldName: 'city', label: 'City', fieldType: 'input', placeholder: 'City', validation: ['Required'], value: ''},
+    {id: 7, fieldName: 'state', label: 'State', fieldType: 'input', placeholder: 'State', validation: ['Required'], value: ''},
+    {id: 8, fieldName: 'postalCode', label: 'Postal Code', fieldType: 'input', placeholder: 'Postal Code', validation: ['Required'], value: ''}
   ]
   
   userForm: FormGroup;
+  dynamicFormGroup: FormGroup;
 
   constructor(private formBuilder: FormBuilder) { }
 
@@ -34,6 +35,13 @@ export class PageLayoutComponent implements OnInit {
       state: new FormControl('', Validators.required),
       postCode: new FormControl('', Validators.required)
     })
+
+    const dynamicFormInputs = {};
+    this.formDynamicInput.forEach(({fieldName, fieldType, value}) => {
+      dynamicFormInputs[fieldName] = new FormControl(value, Validators.required)
+    });
+    this.dynamicFormGroup = this.formBuilder.group(dynamicFormInputs)
+    console.log(this.dynamicFormGroup)
   }
 
   submit(values){
