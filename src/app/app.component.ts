edb789d233fd22231e043f835722caca13510066
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-09-04 16:57:19 +0530

    Passed value from consumer component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  dynamicInputs:any[] = [
+    {id: 1, fieldName: 'firstName', label: 'First Name', fieldType: 'input', placeholder: 'First Name', validation: ['Required'], value: '3453'},
+    {id: 2, fieldName: 'lastName', label: 'Last Name', fieldType: 'input', placeholder: 'Long Last Name That Will Be Truncated', validation: ['Required'], value: ''},
+    {id: 3, fieldName: 'company', label: 'Company', fieldType: 'input', placeholder: 'Company', validation: ['Required'], value: ''},
+    {id: 4, fieldName: 'address', label: 'Address', fieldType: 'textarea', placeholder: 'Address', validation: ['Required'], value: ''},
+    {id: 5, fieldName: 'address2', label: 'Address 2', fieldType: 'textarea',placeholder: 'Address 2', validation: ['Required'], value: ''},
+    {id: 6, fieldName: 'city', label: 'City', fieldType: 'input', placeholder: 'City', validation: ['Required'], value: ''},
+    {id: 7, fieldName: 'state', label: 'State', fieldType: 'input', placeholder: 'State', validation: ['Required'], value: ''},
+    {id: 8, fieldName: 'postalCode', label: 'Postal Code', fieldType: 'input', placeholder: 'Postal Code', validation: ['Required'], value: ''}
+  ]
 }
--- src/app/page-layout/page-layout.component.ts
+++ src/app/page-layout/page-layout.component.ts
@@ -1,35 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, ChangeDetectionStrategy } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators, FormArrayName } from '@angular/forms';
 
 @Component({
   selector: 'da-page-layout',
   templateUrl: './page-layout.component.html',
-  styleUrls: ['./page-layout.component.css']
+  styleUrls: ['./page-layout.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PageLayoutComponent implements OnInit {
+export class PageLayoutComponent implements OnChanges {
+  @Input() dynamicInputs: any[];
   dynamicFormGroup: FormGroup;
-  formDynamicInput:any[] = [
-    {id: 1, fieldName: 'firstName', label: 'First Name', fieldType: 'input', placeholder: 'First Name', validation: ['Required'], value: '3453'},
-    {id: 2, fieldName: 'lastName', label: 'Last Name', fieldType: 'input', placeholder: 'Long Last Name That Will Be Truncated', validation: ['Required'], value: ''},
-    {id: 3, fieldName: 'company', label: 'Company', fieldType: 'input', placeholder: 'Company', validation: ['Required'], value: ''},
-    {id: 4, fieldName: 'address', label: 'Address', fieldType: 'textarea', placeholder: 'Address', validation: ['Required'], value: ''},
-    {id: 5, fieldName: 'address2', label: 'Address 2', fieldType: 'textarea',placeholder: 'Address 2', validation: ['Required'], value: ''},
-    {id: 6, fieldName: 'city', label: 'City', fieldType: 'input', placeholder: 'City', validation: ['Required'], value: ''},
-    {id: 7, fieldName: 'state', label: 'State', fieldType: 'input', placeholder: 'State', validation: ['Required'], value: ''},
-    {id: 8, fieldName: 'postalCode', label: 'Postal Code', fieldType: 'input', placeholder: 'Postal Code', validation: ['Required'], value: ''}
-  ]
-  
+
   constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit() {
+  buildForm () {
     const dynamicFormInputs = {};
-    this.formDynamicInput.forEach(({fieldName, fieldType, value}) => {
+    (this.dynamicInputs || []).forEach(({ fieldName, value }) => {
       dynamicFormInputs[fieldName] = new FormControl(value, Validators.required)
     });
     this.dynamicFormGroup = this.formBuilder.group(dynamicFormInputs)
   }
 
-  submit(values){
+  ngOnChanges(model: SimpleChanges) {
+    if(model.dynamicInputs.currentValue !== model.dynamicInputs.previousValue) {
+      this.buildForm();
+    }
+  }
+
+  submit(values) {
     console.log(values);
   }
 
